5eae49b94649c166844b54ff2bca2eca7617f916
maxxof <maxim.egorov.soccer@gmail.com>
2021-10-12 22:33:53 +0300

    ---->Download<------

--- app/src/main/java/com/example/gymdiary/MainScreen.java
+++ app/src/main/java/com/example/gymdiary/MainScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +14,20 @@ import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainScreen extends AppCompatActivity {
-    private FloatingActionButton floatingButtonPlus;
+    private FloatingActionButton floatingButtonPlus, floatingButtonMinus;
     private Button buttonDay1, buttonDay2, buttonDay3, buttonDay4, buttonDay5, buttonDay6;
     private EditText et;
     private View v;
     private TextView textViewNameSession;
+    private boolean buttonDay1Visible;
+
+    public static final String SHARED_PREFS = "sp";
+    public static final String BUTTONDAY1 = "buttonDay1";
+    public static final String BUTTONDAY2 = "buttonDay2";
+    public static final String BUTTONDAY3 = "buttonDay3";
+    public static final String BUTTONDAY4 = "buttonDay4";
+    public static final String BUTTONDAY5 = "buttonDay5";
+    public static final String BUTTONDAY6 = "buttonDay6";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +35,8 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         Intent intentMainScreen = getIntent();
 
-        SharedPreferences sh = getSharedPreferences("MySharedPreferences", MODE_PRIVATE);
         floatingButtonPlus = findViewById(R.id.floatingButtonPlus);
+        floatingButtonMinus= findViewById(R.id.floatingButtonMinus);
         buttonDay1 = findViewById(R.id.buttonDay1);
         buttonDay2 = findViewById(R.id.buttonDay2);
         buttonDay3 = findViewById(R.id.buttonDay3);
@@ -42,10 +52,63 @@ public class MainScreen extends AppCompatActivity {
         buttonDay4.setVisibility(v.INVISIBLE);
         buttonDay5.setVisibility(v.INVISIBLE);
         buttonDay6.setVisibility(v.INVISIBLE);
+
+        loadData();
+        updateViews();
+    }
+
+    protected void onStart(){
+        super.onStart();
+        loadData();
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        saveData();
+    }
+    protected void onPause(){
+        super.onPause();
+        saveData();
+
+    }
+    protected void onStop(){
+        super.onStop();
+        saveData();
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        saveData();
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+    public void saveData() {
+        SharedPreferences sp = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putBoolean(BUTTONDAY1, buttonDay1.isCursorVisible());
+
+        editor.apply();
+
+    }
+    public void loadData() {
+        SharedPreferences sp = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        buttonDay1Visible = sp.getBoolean(BUTTONDAY1, true);
+
+
+    }
+    public void updateViews() {
+        buttonDay1.setCursorVisible(buttonDay1Visible);
+
     }
 
 
     public void plusClick(View view1) {
+
         if (buttonDay1.getVisibility() == v.INVISIBLE) {
             buttonDay1.setText(et.getText().toString());
             buttonDay1.setVisibility(v.VISIBLE);
@@ -68,6 +131,7 @@ public class MainScreen extends AppCompatActivity {
             buttonDay6.setText(et.getText().toString());
             buttonDay6.setVisibility(v.VISIBLE);
         }
+        saveData();
     }
 
     public void minusClick(View view2) {
@@ -84,6 +148,7 @@ public class MainScreen extends AppCompatActivity {
         } else if (buttonDay1.getVisibility() == v.VISIBLE) {
             buttonDay1.setVisibility(v.INVISIBLE);
         }
+        saveData();
     }
 
     public void buttonDay1Click(View view) {
